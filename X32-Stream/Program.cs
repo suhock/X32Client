bfa3ee76be1837ae5e35aa9a19b4fa3f1c48ebe6
Matthew Suhocki <matt.suhocki@fivetwo.org>
2020-07-08 20:16:03 +0900

    Add project files.

---
+++ X32-Stream/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace X32_Stream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
