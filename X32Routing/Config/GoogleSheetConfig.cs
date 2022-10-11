ad85256dd7b39ce553e7783884daf440d797babe
Matthew Suhocki <matt@fivetwo.org>
2022-10-11 14:14:49 +0900

    Isolated X32Client project

--- X32Routing/Config/GoogleSheetConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Suhock.X32.Routing.Config;
-
-internal sealed class GoogleSheetConfig
-{
-    public string CredentialsFilename { get; set; } = "credentials.json";
-    public string SpreadsheetId { get; set; } = "";
-    public string SpreadsheetRange { get; set; } = "Channels!A1:G";
-}
--- X32Routing/Config/SendConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿using Suhock.X32.Types.Enums;
-
-namespace Suhock.X32.Routing.Config;
-
-internal sealed class SendConfig
-{
-    public int Id { get; set; }
-    public InputTap TapType { get; set; } = InputTap.Group;
-}
